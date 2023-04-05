f154eb71cc678125bda7a03511b0bbb7e3da7f68
xhuazz <a0903600970@gmail.com>
2023-04-05 10:51:31 +0800

    課表

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ from message import *
 from new import *
 from Function import *
 from class123 import *
-from today_timetable *
+from today_timetable import *
 #======這裡是呼叫的檔案內容=====
 
 #======python的函數庫==========
